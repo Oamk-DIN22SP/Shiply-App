ba9be9622f4257926e73c2228da76bfea33c670d
maguitaria <t2glma00@students.oamk.fi>
2023-11-15 15:17:29 +0200

    Add Amazon changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
 import express, { Application } from "express";
-import cors, { CorsOptions } from "cors";
+
 import Routes from './routes';
 
 export default class Server {
@@ -9,11 +9,7 @@ export default class Server {
   }
 
   private config(app: Application): void {
-    const corsOptions: CorsOptions = {
-      origin: "http://localhost:8081"
-    };
 
-    app.use(cors(corsOptions));
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
   }
