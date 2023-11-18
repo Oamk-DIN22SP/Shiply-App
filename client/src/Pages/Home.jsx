42ebb223206b4c96a3da31a2979883c8b088f08e
shovoncse <shovondas26@gmail.com>
2023-11-18 14:25:46 +0200

    :construction: track functionality

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -5,15 +5,28 @@ import Details from '../Components/Details'
 
 export default function Home() {
   const [data, setData] = React.useState(null)
+  const [track, setTrack] = React.useState(false)
 
   const handleNotificationItemClick = (item) => {
+    setTrack(false)
     setData(item)
+  }
+  
+  const handleTrackStatus = (status) => {
+    setTrack(status)
   }
 
   return (
     <div className='home_page'>
       <Notification onNotificationItemClick={handleNotificationItemClick} />
-      <Details selectedItem={data}/>
+      
+      {
+        track? 
+        <div className="track">This is track form component</div> 
+        : 
+        <Details selectedItem={data} handleTrackClick={handleTrackStatus}/>
+      }
+      
     </div>
   )
 }
