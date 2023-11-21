b260fd2f60e6d90bf91ab7fcf415ab0edc459252
RachnaKumar <t2kura00@students.oamk.fi>
2023-11-21 14:20:20 +0200

    Parcel model commit

--- backend/src/models/parcel.model.ts
+++ backend/src/models/parcel.model.ts
@@ -3,11 +3,16 @@ import { RowDataPacket } from "mysql2"
 
 export default interface Parcel extends RowDataPacket {
     parcelID?: number;
+    senderID: number; // which is a firebase id of a user who is signed in now
     receiverID: number;
+   // info of a receiver
+   receiverEmail: string;
+
     driverID: number;
     status: Status;
     parcelDescription?: string;
     deliveryAddress?: string;
+    pickupDate: Date; // date when parcel is picked up
     deliveryDate?: Date;
     deliveryNotes: string;
 }
