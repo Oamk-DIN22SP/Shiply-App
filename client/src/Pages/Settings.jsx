fff80c9eff565cc80145ef301ed1cf803e81938b
RachnaKumar <t2kura00@students.oamk.fi>
2023-11-22 11:03:12 +0200

    “:sparkles:” Setting page

--- client/src/Pages/Settings.jsx
+++ client/src/Pages/Settings.jsx
@@ -3,6 +3,8 @@ import { Button } from "@mui/material";
 import { signOut } from "firebase/auth";
 import { auth } from "../config/firebase.config";
 import { useNavigate } from "react-router-dom";
+import Grid from '@mui/material/Grid';
+
 
 export default function Settings() {
 const navigate = useNavigate();
@@ -11,6 +13,7 @@ const navigate = useNavigate();
     try {
       await signOut(auth);
       console.log("User signed out successfully");
+      alert("User signed out successfully");
        navigate("/login");
 
     } catch (error) {
@@ -19,12 +22,19 @@ const navigate = useNavigate();
   };
 
   return (
-    <div>
-      Settings
-      <br />
-      <Button variant="outlined" color="secondary" onClick={handleSignOut}>
-        Sign out
-      </Button>
+    <div className="details">    
+    <p className='heading' style={{border:'1px solid #FFFAF6', padding:'10px', backgroundColor:'#FFFAF6',borderRadius:'10px 10px 0 0'}}>Settings</p>
+      <Grid style={{ backgroundColor: '#FFFAF6', padding: '10px', borderRadius: '5px',marginTop: '10px', height: '70vh' }}>
+      <h5 className="set_heading">Time is Limited!</h5>
+        <p className='setting_content'>Of course there are a lot of settings should be included. Yet, 
+        we have limited time and compulsory requirements to fulfill. Priorities :D
+         </p>
+          <br>
+          </br>
+          <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
+          <Button variant="contained" onClick={handleSignOut} style={{ backgroundColor: '#BF5000', color: '#FDF9F3' }}>Delete Account</Button>
+        </div>
+      </Grid>
     </div>
   );
 }
