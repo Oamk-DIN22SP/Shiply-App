af5105b0f2590f83328f99c3f2a6476351d9e475
maguitaria <t2glma00@students.oamk.fi>
2023-12-05 17:43:33 +0200

    Add _test for testing APis
    Was breaking Postman tests and current structure

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,15 +4,15 @@ import userRoutes from "./user.routes";
 import DriverRoutes from "./drivers";
 import homeRoutes from "./home.routes";
 import LocationRoutes from "./locations";
-// import ParcelRoutes from './parcels';
+import ParcelRoutes from './parcels'; 
 import ParcelRoutesTest from './parcels_test';
 import AuthRoutes from './auth'
 export default class Routes {
   constructor(app: Application) {
     app.use("/api", homeRoutes);
     app.use("/api/users", userRoutes);
-    // app.use("/api/parcels", ParcelRoutes);
-    app.use("/api/parcels", ParcelRoutesTest);
+    app.use("/api/parcels", ParcelRoutes);
+    app.use("/api_test/parcels", ParcelRoutesTest);
     app.use("/api/driver", DriverRoutes);
     app.use("/api/locations", LocationRoutes);
     app.use("/api/auth", AuthRoutes)
